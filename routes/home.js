93b9b8e96450ad24c4a2ae9e481e0c8f0aaaa201
genebit <genebitara@gmail.com>
2022-04-21 23:32:20 +0800

    Removed routes folder and opt for controllers

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const controller = require("../controllers/homeController")
-router.get("/", controller.index)
-router.get("/about", controller.about)
-router.get("/contacts", controller.contacts)
-
-module.exports = router
